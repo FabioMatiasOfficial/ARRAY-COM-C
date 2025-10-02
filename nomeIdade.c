5a72c2d6a0d8b32563aa5606947216bdd85757ec
FabioMatiasOfficial <Fabio.casco.aquino@escola.pr.gov.br>
2025-10-02 14:52:55 +0000

    1

---
+++ nomeIdade.c
@@ -0,0 +1,14 @@
+int main(){
+    char nome[50]; //array de caracteres para armazenar nome
+    int idade;     // variável para idade
+
+    //entrada:
+    printf("Digite seu nome: ");
+    scanf("%d",&idade);
+
+    print("Olá %s, você tem &d anos!\n", nome,idade);
+
+    if(alguma coisa ){
+        
+    }
+}
